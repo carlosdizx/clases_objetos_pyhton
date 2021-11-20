b9520a9a23b75f6fa1f80a311b003880b1a920f0
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-20 11:08:03 -0500

    Creado instancias de la clase con parametros para crear objetos con sus propios datos

--- Persona.py
+++ Persona.py
@@ -1,5 +1,18 @@
 class Persona:
-    pass
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
 
-print(type(Persona))
+persona1 = Persona('Carlos', 'Díaz', 23)
+print(persona1.nombre)
+print(persona1.apellido)
+print(persona1.edad)
+
+print("-------")
+
+persona2 = Persona('Ernesto', 'Basante', 32)
+print(persona2.nombre)
+print(persona2.apellido)
+print(persona2.edad)
