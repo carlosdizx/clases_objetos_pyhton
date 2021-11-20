a99ef199da03876a372f350f355e0b28e854a0e1
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-20 12:55:20 -0500

    Robustisido el metodo init

--- Persona.py
+++ Persona.py
@@ -1,14 +1,16 @@
 class Persona:
-    def __init__(self, nombre, apellido, edad):
+    def __init__(self, nombre, apellido, edad, *valores, **terminos):
         self.nombre = nombre
         self.apellido = apellido
         self.edad = edad
+        self.valores = valores
+        self.terminos = terminos
 
     def mostrar_detalle(self):
-        return f'{self.nombre} {self.apellido} {self.edad}'
+        return f'{self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}'
 
 
-persona1 = Persona('Carlos', 'Díaz', 23)
+persona1 = Persona('Carlos', 'Díaz', 23, '3163930876', 'B+', 'Guapo', m='Manzana', p='Pera')
 persona2 = Persona('Ernesto', 'Basante', 32)
 
 print(f'Persona 1: {persona1.mostrar_detalle()}')
