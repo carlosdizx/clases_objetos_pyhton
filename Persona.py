dbaffd3b4b8d5d9789228f1f92a98584a9ff2ca7
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-20 12:18:36 -0500

    Agregado un metodo que resume el mostrar infromacion de la entidad Persona y puede ser utilizado por cualquier objeto que instancie a esta clase

--- Persona.py
+++ Persona.py
@@ -4,18 +4,12 @@ class Persona:
         self.apellido = apellido
         self.edad = edad
 
+    def mostrar_detalle(self):
+        return f'{self.nombre} {self.apellido} {self.edad}'
+
 
 persona1 = Persona('Carlos', 'Díaz', 23)
-print(f'Persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
 persona2 = Persona('Ernesto', 'Basante', 32)
-print(f'Persona 2: {persona2.nombre} {persona2.apellido} {persona2.edad}')
 
-print("-------Modificando los datos-------")
-persona1.nombre = "Carlos Ernesto"
-persona1.apellido = "Díaz Basante"
-persona1.edad = "Veintitres años"
-persona2.nombre = "Ernesto Carlos"
-persona2.apellido = "Basante Díaz"
-persona2.edad = 23
-print(f'Persona 1: {persona1.nombre} {persona1.apellido} {persona1.edad}')
-print(f'Persona 2: {persona2.nombre} {persona2.apellido} {persona2.edad}')
+print(f'Persona 1: {persona1.mostrar_detalle()}')
+print(f'Persona 2: {persona2.mostrar_detalle()}')
