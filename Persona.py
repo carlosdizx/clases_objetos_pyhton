78fe2165a9cc889711c9fdfd2408e9ca0873603e
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 20:01:33 -0500

    Creado el get y set del atributo nombre

--- Persona.py
+++ Persona.py
@@ -1,15 +1,19 @@
 class Persona:
     def __init__(self, nombre, apellido, edad, ):
-        self.__nombre = nombre
+        self._nombre = nombre
         self._apellido = apellido
         self._edad = edad
 
     def mostrar_detalle(self):
-        return f'{self.__nombre} {self._apellido} {self._edad}'
+        return f'{self._nombre} {self._apellido} {self._edad}'
 
     @property
     def nombre(self):
-        return self.__nombre
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
 
 
 persona1 = Persona('Carlos', 'Díaz', 23)
