1466ba6a270db533978b0fe39ccd78de2fa5196c
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 20:06:51 -0500

    Creado el get y set de los demas atributos, se los puso a preuba

--- Persona.py
+++ Persona.py
@@ -15,7 +15,26 @@ class Persona:
     def nombre(self, nombre):
         self._nombre = nombre
 
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
+
 
 persona1 = Persona('Carlos', 'Díaz', 23)
-print(persona1.nombre)
-print(persona1.nombre)
+print(f'{persona1.nombre},{persona1.apellido},{persona1.edad}')
+persona1.nombre = "Carlos Ernesto"
+persona1.apellido = "Díaz Basante"
+persona1.edad = "Veititres"
+print(f'{persona1.nombre},{persona1.apellido},{persona1.edad}')
