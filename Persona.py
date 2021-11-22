65efbb9dd1169abfbf2041bb1925065df139455b
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 20:18:37 -0500

    Evitando que se muestre el codigo de prueba si se ejecuta el archivo desde otro

--- Persona.py
+++ Persona.py
@@ -30,3 +30,12 @@ class Persona:
 
     def mostrar_detalle(self):
         return f'{self._nombre} {self._apellido} {self._edad}'
+
+
+if __name__ == '__main__':
+    persona1 = Persona('Carlos', 'Díaz', 23)
+    print(persona1.mostrar_detalle())
+    persona1.nombre = "Carlos Ernesto"
+    persona1.apellido = "Díaz Basante"
+    persona1.edad = "Veititres"
+    print(persona1.mostrar_detalle())
